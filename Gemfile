199e89ea0c2de02193eb2c5ffc12adc99b9346e0
Mccxiv <adr.stella@gmail.com>
2022-06-11 20:50:46 +0800

    Use sinatra

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
 source 'https://rubygems.org'
-gem 'webrick', '1.7.0'
+gem 'sinatra', '2.2.0'
 gem 'income-tax', '0.3.2'
--- api.rb
+++ api.rb
@@ -1,58 +1,47 @@
 require 'json'
-require 'webrick'
+require 'sinatra'
 require 'income-tax'
 
-class MyServlet < WEBrick::HTTPServlet::AbstractServlet
-  def do_GET (request, response)      
-    result = nil
-    status = 200
+set :port, (ENV['PORT'] or 3230)
 
-    case request.path
-      when "/estimate"
-        country = request.query["country"]
-        yearly = request.query["yearly"]
-        if country && yearly
-          begin
-            income_tax = IncomeTax.new(country, yearly)
-            result_hash = { 
-              :gross_income => income_tax.gross_income,
-              :net_income => income_tax.net_income.to_f,
-              :taxes => income_tax.taxes.to_f,
-              :rate => income_tax.to_f,
-              :rate_string => income_tax.rate
-            }
-            puts result_hash
-            result = JSON.pretty_generate(result_hash)
-          rescue => e
-            status = 500
-            result = e.message
-          end
-        else
-          status = 400
-          result = "missing parameters"
-        end
-      when "/healthcheck"
-        result = "ok"
-      when '/'
-        result = "API is running. Try /estimate"
-      else
-        status = 404
-        result = "no such path"
-    end 
+get '/' do
+  'API is running. Try /estimate'
+end
 
-    response.status = status
-    response.body = result.to_s + "\n"
+get '/estimate' do
+  country = params[:country]
+  yearly = params[:yearly]
+
+  if country && yearly
+    begin
+      income_tax = IncomeTax.new(country, yearly)
+      result_hash = { 
+        :gross_income => income_tax.gross_income,
+        :net_income => income_tax.net_income.to_f,
+        :taxes => income_tax.taxes.to_f,
+        :rate => income_tax.to_f,
+        :rate_string => income_tax.rate
+      }
+      json = JSON.pretty_generate(result_hash)
+      content_type :json
+      json      
+    rescue => e
+      status 500
+      e.message
+    end
+  else
+    status 400
+    'missing parameters'
   end
 end
 
-server = WEBrick::HTTPServer.new(:Port => (ENV['PORT'] or 3230))
-
-server.mount "/", MyServlet
+get '/*' do
+  status 400
+  'nothing on this path'
+end
 
 trap("INT") {
   server.shutdown
 }
 
-puts "Starting API server 🚀"
-
-server.start
+puts "API server started 🚀"
